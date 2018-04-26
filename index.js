1109b7d44b651c7432361c9311eda59ec656a612
forafish <fora.luo@gmail.com>
2018-04-26 19:12:37 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require = require("esm")(module/*, options*/);
-console.time('mostly-blob-storage import');
 module.exports.localStorage = require('./src/multer/local_storage').default;
 module.exports.localBlobStore = require('./src/local_blob_store').default;
 module.exports.minioStorage = require('./src/multer/minio_storage').default;
@@ -7,4 +6,3 @@ module.exports.minioBlobStore = require('./src/minio_blob_store').default;
 module.exports.serveMinio = require('./src/express/serve_minio').default;
 module.exports.s3Storage = require('./src/multer/s3_storage').default;
 module.exports.s3BlobStore = require('./src/s3_blob_store').default;
-console.timeEnd('mostly-blob-storage import');
