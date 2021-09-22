ac31eb5410551e57666a515436e44487bdf414b8
anton <dasantonym@users.noreply.github.com>
2021-09-22 11:15:59 +0200

    Use stream directly with minio client, do not copy to buffer

--- src/minio_blob_store.js
+++ src/minio_blob_store.js
@@ -40,23 +40,16 @@ class MinioBlobStore {
     assert(opts.key, 'opts.key is not provided');
     assert(bucket, 'opts.bucket is not provided');
 
-    let bufferStream = new stream.PassThrough();
-    let buffer = new Buffer.alloc(0);
+    let passThrough = new stream.PassThrough();
 
-    bufferStream.on('data', chunk => {
-      buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length);
+    this.client.putObject(bucket, opts.key, passThrough, (err, etag) => {
+      if (err) return cb(err);
+      return cb(null, { etag: etag });
     });
 
-    bufferStream.on('end', () => {
-      this.client.putObject(bucket, opts.key, buffer, (err, etag) => {
-        if (err) return cb(err);
-        return cb(null, { etag: etag });
-      });
-    });
+    passThrough.on('error', cb);
 
-    bufferStream.on('error', cb);
-
-    return bufferStream;
+    return passThrough;
   }
 
 
